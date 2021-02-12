3920de83fc7d0cf66c0afb7ea60b089a6c49c84e
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-12 01:25:23 -0500

    feat/mobile-first Add home mobile structure HTML light theme

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Variables
+const inputSearch = document.querySelector(".input-search");
+
+// Functions
+function handleInputSearch(e) {
+	console.log(e);
+}
+
+// Events
+inputSearch.addEventListener("keypress", handleInputSearch);
