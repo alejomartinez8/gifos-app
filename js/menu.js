18fa34b58199b3348d646e4cc03809fb7890a2c5
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-13 18:19:23 -0500

    Add My Gifos Pages

--- js/menu.js
+++ js/menu.js
@@ -34,24 +34,23 @@ function setTheme(e) {
 	if (e) e.preventDefault();
 	document.body.classList.toggle("dark");
 	theme = document.body.classList.value;
-	console.log(theme);
+
 	if (theme === "dark") {
 		localStorage.setItem("theme", "dark");
-		if (menuOpen)
-			setAttribute(mainMenuButton, "src", "./img/close-modo-noct.svg");
-		else mainMenuButton.setAttribute("src", "./img/burger-modo-noct.svg");
-		toggleTheme.innerHTML = "Modo Diurno";
+		setAttribute(
+			mainMenuButton,
+			"src",
+			menuOpen ? "./img/close-modo-noct.svg" : "./img/burger-modo-noct.svg"
+		);
 		setAttribute(searchIcon, "src", "./img/icon-search-mod-noc.svg");
-		// TODO - when be desktop
 		setAttribute(logoImg, "src", "./img/logo-mobile-modo-noct.svg");
+		toggleTheme.innerHTML = "Modo Diurno";
 	} else {
 		localStorage.setItem("theme", "");
-		if (menuOpen) setAttribute(mainMenuButton, "src", "./img/close.svg");
-		else setAttributeNS(mainMenuButton, "src", "./img/burger.svg");
-		toggleTheme.innerHTML = "Modo Nocturno";
+		setAttribute(mainMenuButton, menuOpen && "src", "./img/close.svg");
 		setAttribute(searchIcon, "src", "./img/icon-search.svg");
-		// TODO - when be desktop
 		setAttribute(logoImg, "src", "./img/logo-mobile.svg");
+		toggleTheme.innerHTML = "Modo Nocturno";
 	}
 }
 
