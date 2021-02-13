b2b7c3c17aa793490ed37921074238a9fbc66cf2
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-13 15:41:21 -0500

    Change img src on toggle theme

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,53 @@
-const mainMenu = document.getElementById("main-menu-button");
+const mainMenuButton = document.getElementById("main-menu-button");
 const navMenu = document.getElementById("nav-menu");
+const toggleTheme = document.getElementById("toggle-theme-button");
+const searchIcon = document.getElementById("search-icon");
+const logoImg = document.getElementById("img-logo");
 
+let theme = "";
 let menuOpen = navMenu.style.display === "block";
 
-mainMenu.addEventListener("click", () => {
-	menuOpen = !menuOpen;
+if (localStorage.getItem("theme") === "dark") {
+	setTheme();
+}
 
+function toggleMenu() {
+	menuOpen = !menuOpen;
 	navMenu.style.display = menuOpen ? "block" : "none";
-	mainMenu.setAttribute(
+	mainMenuButton.setAttribute(
 		"src",
-		menuOpen ? "./img/close.svg" : "./img/burger.svg"
+		theme === "dark"
+			? menuOpen
+				? "./img/close-modo-noct.svg"
+				: "./img/burger-modo-noct.svg"
+			: menuOpen
+			? "./img/close.svg"
+			: "./img/burger.svg"
 	);
-});
+}
+
+function setTheme(e) {
+	if (e) e.preventDefault();
+	document.body.classList.toggle("dark");
+	theme = document.body.classList.value;
+	console.log(theme);
+	if (theme === "dark") {
+		localStorage.setItem("theme", "dark");
+		if (menuOpen) mainMenuButton.setAttribute("src", "./img/close-modo-noct.svg");
+		else mainMenuButton.setAttribute("src", "./img/burger-modo-noct.svg");
+		toggleTheme.innerHTML = "Modo Diurno";
+		searchIcon.setAttribute("src", "./img/icon-search-mod-noc.svg");
+		// TODO - if when be desktop
+		logoImg.setAttribute("src", "./img/logo-mobile-modo-noct.svg");
+	} else {
+		localStorage.setItem("theme", "");
+		if (menuOpen) mainMenuButton.setAttribute("src", "./img/close.svg");
+		else mainMenuButton.setAttributeNS("src", "./img/burger.svg");
+		toggleTheme.innerHTML = "Modo Nocturno";
+		searchIcon.setAttribute("src", "./img/icon-search.svg");
+		logoImg.setAttribute("src", "./img/logo-mobile.svg");
+	}
+}
+
+mainMenuButton.addEventListener("click", toggleMenu);
+toggleTheme.addEventListener("click", setTheme);
