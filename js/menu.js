6a2cbe2550109ded5827b790d95f1efacb7bfaa8
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-13 08:09:31 -0500

    feat/mobile add main menu toggle

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const mainMenu = document.getElementById("main-menu-button");
+const navMenu = document.getElementById("nav-menu");
+
+let menuOpen = navMenu.style.display === "block";
+
+mainMenu.addEventListener("click", () => {
+	menuOpen = !menuOpen;
+	console.log(menuOpen);
+
+	navMenu.style.display = menuOpen ? "block" : "none";
+	mainMenu.setAttribute(
+		"src",
+		menuOpen ? "./img/close.svg" : "./img/burger.svg"
+	);
+});
