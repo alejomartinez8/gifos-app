3b8a0dbaed73aaba9a88a8f2995f98b5e4535935
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-15 07:04:21 -0500

    Add hover to trending gifos

--- js/trending.js
+++ js/trending.js
@@ -1,3 +1,4 @@
+// Slider
 const buttonSliderLeft = document.querySelector(
 	'img[alt = "button-slider-left"]'
 );
@@ -17,3 +18,45 @@ imgHover(
 	"./img/Button-Slider-right-hover.svg",
 	"./img/Button-Slider-right.svg"
 );
+
+// Img Hover
+const gifosTrending = document.querySelector(".gifos-images").children;
+console.log(gifosTrending);
+
+for (let i = 0; i < gifosTrending.length; i++) {
+	gifoHover(gifosTrending[i]);
+}
+
+function gifoHover(elm) {
+	elm.addEventListener("mouseenter", () => {
+		const img = elm.firstElementChild;
+		let divHover = document.createElement("div");
+		divHover.classList.add("hover-img");
+		divHover.innerHTML = `
+        <div class="icon-container">
+            <img src="./img/icon-fav.svg" alt="icon-fav" />
+            <img src="./img/icon-download.svg" alt="icon-download" />
+            <img src="./img/icon-max-normal.svg" alt="" />
+        </div>
+        <div class="text-container">
+            <p class="user">User</p>
+            <p class="title">Titulo GIFO</p>
+        </div>
+        `;
+
+		elm.insertBefore(divHover, img);
+
+		const icons = divHover.children[0].children;
+
+		for (let i = 0; i < icons.length; i++) {
+			icons[i].addEventListener("click", () => {
+				console.log(i, img); // TODO - call function
+			});
+		}
+	});
+
+	elm.addEventListener("mouseleave", () => {
+		const divHover = elm.firstElementChild;
+		elm.removeChild(divHover);
+	});
+}
