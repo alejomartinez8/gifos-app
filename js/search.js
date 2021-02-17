d50921d83a012272512bf3f68d7e559cb1f721f6
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-17 18:06:19 -0500

    Add fetch Search Suggested

--- js/search.js
+++ js/search.js
@@ -2,11 +2,37 @@ const searchContainer = document.getElementById("search-container");
 const inputSearch = document.getElementById("input-search");
 const suggestedContainer = document.getElementById("suggested-container");
 
-inputSearch.addEventListener("input", (e) => {
-  suggestedContainer.style.display = "block";
+const API_KEY = "4SBXaLwgUjTtP0BLoT3mnAk5FL86H0qQ";
 
+async function getSuggestedWords(term) {
+  return await (
+    await fetch(
+      `http://api.giphy.com/v1/tags/related/${term}?api_key=${API_KEY}`
+    )
+  ).json();
+}
+
+inputSearch.addEventListener("input", async (e) => {
   console.log(e.target.value);
-  if (e.target.value === "") {
+
+  if (e.target.value.length > 1) {
+    const suggestedWords = await getSuggestedWords(e.target.value);
+    if (suggestedWords.data.length) {
+      suggestedContainer.style.display = "block";
+      suggestedContainer.innerHTML = "";
+      console.log(suggestedWords);
+      for (let i = 0; (i < 4) & (i < suggestedWords.data.length); i++) {
+        const term = document.createElement("li");
+        term.innerText = suggestedWords.data[i]?.name;
+        term.tabIndex = 0;
+        suggestedContainer.appendChild(term);
+      }
+    } else {
+      suggestedContainer.style.display = "none";
+      suggestedContainer.innerHTML = "";
+    }
+  } else {
     suggestedContainer.style.display = "none";
+    suggestedContainer.innerHTML = "";
   }
 });
