50a5d44e8e22a94c2770814d9c1f891875d41bae
Alejandro Martinez <alejomartinez8@gmail.com>
2021-02-18 00:40:18 -0500

    Add Section results

--- js/helper.js
+++ js/helper.js
@@ -46,9 +46,14 @@ function imgHover(elm, hover, unhover) {
   });
 }
 
-function gifoHover(elm) {
-  elm.addEventListener("mouseenter", () => {
-    const img = elm.firstElementChild;
+function gifoHover(
+  imgContainer,
+  title = "Titulo GIFO",
+  username = "User",
+  image = ""
+) {
+  imgContainer.addEventListener("mouseenter", () => {
+    const img = imgContainer.firstElementChild;
     let divHover = document.createElement("div");
     divHover.classList.add("hover-img");
     divHover.innerHTML = `
@@ -58,15 +63,15 @@ function gifoHover(elm) {
             <img src="./img/icon-max-normal.svg" alt="" />
         </div>
         <div class="text-container">
-            <p class="user">User</p>
-            <p class="title">Titulo GIFO</p>
+            <p class="user">${username}</p>
+            <p class="title">${title}</p>
         </div>
         `;
 
     divHover.style.width = `${img.offsetWidth}px`;
     divHover.style.height = `${img.offsetHeight}px`;
 
-    elm.insertBefore(divHover, img);
+    imgContainer.insertBefore(divHover, img);
 
     const iconFav = divHover.children[0].children[0];
     const iconDownload = divHover.children[0].children[1];
@@ -82,21 +87,25 @@ function gifoHover(elm) {
 
     iconExpand.addEventListener("click", () => {
       modalContainer.style.display = "flex";
-      const imageModal = document.getElementById("image-modal");
-      setAttribute(imageModal, "src", img.getAttribute("src"));
+      const modalImage = document.getElementById("image-modal");
+      const modalUser = document.getElementById("modal-user");
+      const modalTitle = document.getElementById("modal-title");
+      setAttribute(modalImage, "src", image);
+      modalUser.innerText = username;
+      modalTitle.innerText = title;
     });
 
     if (window.matchMedia("(max-width:768px")) {
       img.addEventListener("click", () => {
         modalContainer.style.display = "flex";
-        const imageModal = document.getElementById("image-modal");
-        setAttribute(imageModal, "src", img.getAttribute("src"));
+        const modalImage = document.getElementById("image-modal");
+        setAttribute(modalImage, "src", img.getAttribute("src"));
       });
     }
   });
 
-  elm.addEventListener("mouseleave", () => {
-    const divHover = elm.firstElementChild;
-    elm.removeChild(divHover);
+  imgContainer.addEventListener("mouseleave", () => {
+    const divHover = imgContainer.firstElementChild;
+    imgContainer.removeChild(divHover);
   });
 }
--- js/search.js
+++ js/search.js
@@ -1,15 +1,25 @@
 const inputSearch = document.getElementById("input-search");
+const iconSearch = document.getElementById("icon-search");
+const sectionResults = document.getElementById("section-results");
+const resultContainer = document.getElementById("container-gifos-result");
+const headingResult = document.getElementById("heading-gifos-result");
+const buttonViewMore = document.getElementById("btn-view-more");
 
 const API_KEY = "4SBXaLwgUjTtP0BLoT3mnAk5FL86H0qQ";
 
 async function getSuggestedWords(term) {
-  return await (
-    await fetch(
-      `https://api.giphy.com/v1/tags/related/${term}?api_key=${API_KEY}`
-    )
-  ).json();
+  return await fetch(
+    `https://api.giphy.com/v1/tags/related/${term}?api_key=${API_KEY}`
+  ).then((response) => response.json());
 }
 
+async function getSearchedGifos(term, limit = 12, offset = 0) {
+  return await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${term}&limit=${limit}&offset=${offset}`
+  ).then((response) => response.json());
+}
+
+// Add Suggested Words to Searcher
 inputSearch.addEventListener("input", async (e) => {
   const suggestedContainer = document.getElementById("suggested-container");
   const lineInputBottom = document.getElementById("line-input-bottom");
@@ -24,6 +34,10 @@ inputSearch.addEventListener("input", async (e) => {
         term.innerText = suggestedWords.data[i]?.name;
         term.tabIndex = 0;
         suggestedContainer.appendChild(term);
+        term.addEventListener("click", () => {
+          inputSearch.value = term.innerText;
+          inputSearch.focus();
+        });
       }
     } else {
       displayNone(suggestedContainer);
@@ -35,4 +49,48 @@ inputSearch.addEventListener("input", async (e) => {
     displayNone(lineInputBottom);
     suggestedContainer.innerHTML = "";
   }
+});
+
+// Add Searched gifos
+async function addSearchedGifos(term, options = { viewMore: false }) {
+  try {
+    const searchedGifos = await getSearchedGifos(term);
+    // console.log(searchedGifos);
+    displayBlock(sectionResults);
+    headingResult.textContent = inputSearch.value;
+
+    searchedGifos.data.forEach((gifo) => {
+      console.log(gifo);
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("img-container");
+
+      const img = document.createElement("img");
+      img.setAttribute("src", gifo.images?.downsized?.url);
+      img.setAttribute("alt", gifo.title);
+
+      imageContainer.appendChild(img);
+      gifoHover(
+        imageContainer,
+        gifo.title,
+        gifo.username,
+        gifo.images?.original?.url
+      );
+      resultContainer.appendChild(imageContainer);
+    });
+
+    if (!options.viewMore) {
+      sectionResults.scrollIntoView({ behavior: "smooth" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+iconSearch.addEventListener("click", () => {
+  resultContainer.innerHTML = "";
+  addSearchedGifos(inputSearch.value);
+});
+
+buttonViewMore.addEventListener("click", () => {
+  addSearchedGifos(inputSearch.value, { viewMore: true });
 });
